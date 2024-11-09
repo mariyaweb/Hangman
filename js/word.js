8ab0ae118ee4fd34c2e269594b9c29f35a14c581
github-mariyaweb <96mariyask@gmail.com>
2024-11-09 19:26:43 +0300

    feat: add copy hangman app

---
+++ js/word.js
@@ -0,0 +1,59 @@
+const words = [
+  {
+    word: 'Algorithm',
+    hint: 'Process or set of rules to be followed in calculations or other problem-solving operations',
+  },
+  {
+    word: 'callback',
+    hint: 'A function that is passed as an argument to another function and is executed when the receiving function completes its task.',
+  },
+  {
+    word: 'closure',
+    hint: 'Feature of JavaScript that allows inner functions to access the outer scope of a function',
+  },
+  {
+    word: 'stack',
+    hint: 'Last in first out Data Structure',
+  },
+  {
+    word: 'unary',
+    hint: 'Special operators that consider a single operand and perform all the operations on that single operand',
+  },
+  {
+    word: 'sprint',
+    hint: 'Timeboxed iteration of a continuous project development cycle—short repeatable phases that last between one and four weeks',
+  },
+  {
+    word: 'server',
+    hint: 'Specialized computer or software system designed to provide services, data, or resources to other computers, known as clients, over a network',
+  },
+  {
+    word: 'repository',
+    hint: 'Centralized storage location for software packages',
+  },
+  {
+    word: 'reflection',
+    hint: 'The ability of a computer program to examine and modify the structure and behavior of a program at runtime. ',
+  },
+  {
+    word: 'Recursion',
+    hint: 'A technique for solving problems when the result depends on solutions to the lesser versions of the same problem',
+  },
+  {
+    word: 'proxy',
+    hint: 'An object that wraps another object and intercepts operations, like reading/writing properties and others, optionally handling them on its own, or transparently allowing the object to handle them.',
+  },
+  {
+    word: 'promise',
+    hint: 'Object that allows you to make asynchronous calls.',
+  },
+  {
+    word: 'interpreter',
+    hint: 'A computer program that is used to directly execute program instructions written using one of the many high-level programming languages',
+  },
+  {
+    word: 'methods',
+    hint: 'Functions that are associated with a particular object',
+  },
+]
+export default words;
